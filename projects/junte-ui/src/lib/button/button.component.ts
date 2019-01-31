de0e9549fc1b441a893a8e564c025e9f7ec27984
juliaselezneva <juli.89@mail.ru>
2019-01-31 15:21:29 +0300

    #13 introduce grid system

--- projects/junte-ui/src/lib/button/button.component.ts
+++ projects/junte-ui/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ju-button',
@@ -6,6 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+
+  @Input() loading = false;
 
   constructor() {
   }
