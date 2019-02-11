1c6978d0b6787606b3a8da6b9ca8f6314881c513
juliaselezneva <juli.89@mail.ru>
2019-02-11 16:40:45 +0300

    #6 radio component

--- projects/junte-ui/src/lib/components/radio/radio.component.ts
+++ projects/junte-ui/src/lib/components/radio/radio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {UI} from '../../enum/ui';
 
 @Component({
   selector: 'jnt-radio',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radio.component.scss']
 })
 export class RadioComponent implements OnInit {
+
+  ui = UI;
+
+  @HostBinding('attr.disabled')
+  @Input()
+  disabled = false;
+
+  @HostBinding('attr.checked')
+  @Input()
+  checked = false;
 
   constructor() { }
 
