a83d884c2d3ab3afebc31150924f52069bc8059b
juliaselezneva <juli.89@mail.ru>
2019-02-21 15:42:51 +0300

    #36 app layout base

---
+++ projects/junte-ui/src/lib/components/app-layout/app-content/app-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jnt-app-content',
+  templateUrl: './app-content.component.html',
+  styleUrls: ['./app-content.component.scss']
+})
+export class AppContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/tests.routes.ts
+++ src/app/components/tests.routes.ts
@@ -1,17 +1,18 @@
-import { Routes } from '@angular/router';
-import { PagerTestComponent } from 'components/pager/pager-test.component';
-import { ButtonsTestComponent } from 'components/buttons/buttons-test.component';
-import { GridTestComponent } from 'components/grid/grid-test.component';
-import { SwitchTestComponent } from 'components/switch/switch-test.component';
-import { StackTestComponent } from 'components/stack/stack-test.component';
-import { SpinnerTestComponent } from 'components/spinner/spinner-test.component';
-import { BlocksTestComponent } from 'components/blocks/blocks-test.component';
-import { CheckboxTestComponent } from 'components/checkbox/checkbox-test.component';
-import { RadioTestComponent } from 'components/radio/radio-test.component';
-import { InputTestComponent } from 'components/input/input-test.component';
+import {Routes} from '@angular/router';
+import {PagerTestComponent} from 'components/pager/pager-test.component';
+import {ButtonsTestComponent} from 'components/buttons/buttons-test.component';
+import {GridTestComponent} from 'components/grid/grid-test.component';
+import {SwitchTestComponent} from 'components/switch/switch-test.component';
+import {StackTestComponent} from 'components/stack/stack-test.component';
+import {SpinnerTestComponent} from 'components/spinner/spinner-test.component';
+import {BlocksTestComponent} from 'components/blocks/blocks-test.component';
+import {CheckboxTestComponent} from 'components/checkbox/checkbox-test.component';
+import {RadioTestComponent} from 'components/radio/radio-test.component';
+import {InputTestComponent} from 'components/input/input-test.component';
 import {FormTestComponent} from 'components/form/form-test.component';
 import {IconTestComponent} from 'components/icon/icon-test.component';
 import {BadgeTestComponent} from 'components/badge/badge-test.component';
+import {AppLayoutTestComponent} from 'components/app-layout/app-layout-test.component';
 
 export const TESTS_ROUTES: Routes = [
   {
@@ -65,6 +66,10 @@ export const TESTS_ROUTES: Routes = [
   {
     path: 'badge',
     component: BadgeTestComponent
+  },
+  {
+    path: 'layout',
+    component: AppLayoutTestComponent
   },
   {
     path: '',
