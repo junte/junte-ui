e815dd16f1cb2690e6b5a416e6a57a30d8a67e11
Vladimir Smirnov <ex700380@gmail.com>
2019-02-26 11:44:29 +0300

    #39 pager in table component

--- src/app/components/pager/pager-test.component.ts
+++ src/app/components/pager/pager-test.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UI } from 'projects/junte-ui/src/lib/enum/ui';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pager-test',
   templateUrl: './pager-test.component.html',
   styleUrls: ['./pager-test.component.scss']
 })
-export class PagerTestComponent {
+export class PagerTestComponent implements OnInit {
   ui = UI;
 
+  form: FormGroup;
   selectedPage = 1;
   pagesCount = 30;
   pageSize = 10;
-  size = 3;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      page: [1]
+    });
+  }
 }
