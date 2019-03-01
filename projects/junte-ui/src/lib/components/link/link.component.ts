d4dcec1b53d58193a0dba850d33ca82ccfc200bb
Vladimir Smirnov <ex700380@gmail.com>
2019-03-01 09:29:47 +0300

    #47 validate form

---
+++ projects/junte-ui/src/lib/components/link/link.component.ts
@@ -0,0 +1,18 @@
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'jnt-link',
+  templateUrl: './link.component.html',
+  styleUrls: ['./link.component.scss']
+})
+export class LinkComponent implements OnInit {
+
+  @Input() source: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
