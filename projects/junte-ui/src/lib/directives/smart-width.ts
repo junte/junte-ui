9ceb5068da8a0b221e90ad3f33c2601d93a487c5
juliaselezneva <juli.89@mail.ru>
2019-03-01 11:48:31 +0300

    #49 markup select component

--- projects/junte-ui/src/lib/directives/smart-width.ts
+++ projects/junte-ui/src/lib/directives/smart-width.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 const GUTTER_SIZE = 9;
 
@@ -15,32 +15,47 @@ export class SmartWidthDirective implements AfterViewInit {
     this.host = host.nativeElement;
   }
 
+
   ngAfterViewInit() {
     this.measure = this.renderer.createElement('span');
     this.renderer.setStyle(this.measure, 'opacity', '0');
+    this.renderer.setStyle(this.measure, 'border', '1px solid red');
     this.renderer.setStyle(this.measure, 'height', '0');
     this.renderer.setStyle(this.measure, 'whiteSpace', 'pre');
     this.renderer.setStyle(this.measure, 'fontFamily', this.host.style.fontFamily);
     this.renderer.appendChild(this.host.parentElement, this.measure);
-    this.updateWidth();
+    console.log('old div', this.measure.offsetWidth);
+    console.log('old input', this.host.offsetWidth);
+    this.updateWidth(this.measure.offsetWidth);
   }
+
 
   @HostListener('keyup') changed() {
-    this.updateWidth();
-  }
-
-  private updateWidth() {
+    const old = this.measure.offsetWidth;
+    console.log('old', old);
+    console.log(this.measure.offsetWidth);
+    console.log(this.host.offsetWidth);
     this.measure.innerHTML = this.host.value;
 
-    const style = getComputedStyle(this.host, null);
-    this.renderer.setStyle(this.measure, 'fontSize', style.getPropertyValue('font-size'));
-    this.renderer.setStyle(this.measure, 'padding', style.getPropertyValue('padding'));
+    // if (this.host.offsetWidth < (this.measure.offsetWidth + 0.5)) {
+    this.updateWidth(old);
 
-    const width = this.host.getBoundingClientRect().width + (this.measure.getBoundingClientRect().width - this.host.getBoundingClientRect().width) + 0.5;
+    // }
+  }
+
+  private updateWidth(old) {
+    const style = getComputedStyle(this.host, null);
+
+
+    //this.renderer.setStyle(this.measure, 'fontSize', style.getPropertyValue('font-size'));
+    //this.renderer.setStyle(this.measure, 'padding', style.getPropertyValue('padding'));
+
+    console.log('div', this.measure.offsetWidth);
+    console.log('input', this.host.offsetWidth);
+
+
+    const width = this.host.offsetWidth + (this.measure.offsetWidth - old) + 0.5;
     this.renderer.setStyle(this.host, 'width', `${width}px`);
 
-
-    console.log('div', this.measure.getBoundingClientRect().width);
-    console.log('input', this.host.getBoundingClientRect().width);
   }
 }
