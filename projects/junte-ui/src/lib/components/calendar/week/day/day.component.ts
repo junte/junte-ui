00f7f8769d9db83eccfe641d0b0f74dcc36186f9
juliaselezneva <juli.89@mail.ru>
2019-03-22 13:55:31 +0300

    #64 week markup

---
+++ projects/junte-ui/src/lib/components/calendar/week/day/day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jnt-calendar-day',
+  templateUrl: './day.component.html',
+  styleUrls: ['./day.component.scss']
+})
+export class DayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
