0082977cb4d5fb1090932d5af3ddbc656efea26a
juliaselezneva <juli.89@mail.ru>
2019-03-26 10:34:10 +0300

    #69 refactor calendar week

--- projects/junte-ui/src/lib/components/stack/stack.component.ts
+++ projects/junte-ui/src/lib/components/stack/stack.component.ts
@@ -1,5 +1,5 @@
-import {Component, HostBinding, Input} from '@angular/core';
-import {StackType, StackGutter} from '../../enum/ui';
+import { Component, HostBinding, Input } from '@angular/core';
+import { StackType, StackGutter, FlexAlign, FlexJustify, FlexDirection, FlexWrap } from '../../enum/ui';
 
 @Component({
   selector: 'jnt-stack',
@@ -9,8 +9,21 @@ import {StackType, StackGutter} from '../../enum/ui';
 export class StackComponent {
 
   @HostBinding('attr.gutter')
-  @Input() gutter:  StackGutter = StackGutter.normal;
+  @Input() gutter: StackGutter = StackGutter.normal;
 
   @HostBinding('attr.type')
   @Input() type: StackType = StackType.vertical;
+
+  @HostBinding('attr.align')
+  @Input()
+  align: FlexAlign = FlexAlign.start;
+
+  @HostBinding('attr.justify')
+  @Input()
+  justify: FlexJustify = FlexJustify.start;
+
+  @HostBinding('attr.wrap')
+  @Input()
+  wrap: FlexWrap = FlexWrap.noWrap;
+
 }
