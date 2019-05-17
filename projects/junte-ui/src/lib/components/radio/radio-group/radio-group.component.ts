b9457b9cff7215b6e4fa9cf0d742f4a2ecfa9b10
Vladimir Smirnov <ex700380@gmail.com>
2019-05-17 14:28:14 +0000

    Revert "#155 improve radio group component"
    This reverts commit 9887011

--- projects/junte-ui/src/lib/components/radio/radio-group/radio-group.component.ts
+++ projects/junte-ui/src/lib/components/radio/radio-group/radio-group.component.ts
@@ -4,13 +4,15 @@ import {
   ContentChildren,
   forwardRef,
   HostBinding,
+  Input,
   OnDestroy,
   QueryList
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { RadioComponent } from '../radio.component';
-import { fromEvent, merge, Subscription } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { BehaviorSubject, fromEvent, merge, Subscription } from 'rxjs';
+import { filter, mapTo } from 'rxjs/operators';
+import { isObject, isUndefined } from 'util';
 
 @Component({
   selector: 'jnt-radio-group',
@@ -30,34 +32,56 @@ export class RadioGroupComponent implements AfterContentInit, OnDestroy {
 
   @ContentChildren(RadioComponent, {descendants: true}) listRadioComponent: QueryList<RadioComponent>;
 
+  @Input() labelField: string;
+  @Input() valueField: string;
+
   private subscribe: Subscription;
-  private _selected: any;
+  private selected$: BehaviorSubject<any> = new BehaviorSubject<any>(undefined);
 
   set selected(value: any) {
-    this._selected = value;
-
-    this.updateSelected();
-    this.onChange(value);
+    this.selected$.next(value);
   }
 
   get selected() {
-    return this._selected;
+    return this.selected$.getValue();
+  }
+
+  constructor() {
   }
 
   ngAfterContentInit() {
+    setTimeout(() => this.transformationRadio());
+
     const changes$ = this.listRadioComponent.map(radio =>
       fromEvent(radio.getElement(), 'click').pipe(mapTo(radio)));
 
     this.subscribe = merge(...changes$).subscribe((radio: RadioComponent) =>
       this.selected = radio.value);
+
+    this.selected$.pipe(filter(v => !isUndefined(v)))
+      .subscribe(value => {
+        this.onChange(value);
+        this.updateSelected();
+      });
   }
 
   ngOnDestroy() {
     this.subscribe.unsubscribe();
   }
 
+  private transformationRadio() {
+    this.listRadioComponent.forEach(r => {
+      if (isObject(r.value)) {
+        r.label = r.value[this.labelField];
+        r.value = r.value[this.valueField];
+      }
+    });
+    this.updateSelected();
+  }
+
   private updateSelected() {
-    this.listRadioComponent.forEach(r => r.checked = r.value === this.selected);
+    this.listRadioComponent.forEach(r =>
+      r.checked = r.value === this.selected);
   }
 
   writeValue(value: any) {
--- src/app/components/radio/radio-test.component.ts
+++ src/app/components/radio/radio-test.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UI } from 'projects/junte-ui/src/lib/enum/ui';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-radio-test',
   templateUrl: './radio-test.component.html',
   styleUrls: ['./radio-test.component.scss']
 })
-export class RadioTestComponent {
+export class RadioTestComponent implements OnInit {
   ui = UI;
 
+  form: FormGroup;
+
+  developers = [
+    {developer: 'Anton', age: 32},
+    {developer: 'Andrey', age: 33},
+    {developer: 'Roman', age: 26}
+  ];
   selected = 2;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      select: this.fb.control(5),
+      selectDeveloper: this.fb.control(26)
+    });
+  }
 }
