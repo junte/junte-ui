eabb429814d560ff0bb30873a8a0a4ac856e5678
juliaselezneva <juli.89@mail.ru>
2019-06-06 12:00:26 +0300

    #206 implement function for contrast colors in label

--- projects/junte-ui/src/lib/components/label/label.component.ts
+++ projects/junte-ui/src/lib/components/label/label.component.ts
@@ -1,11 +1,14 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { Colors, Icons } from '../../enum/ui';
+
+const LIGHT_COLOR = '#FFF';
+const DARK_COLOR = '#4F4F4F';
 
 @Component({
   selector: 'jnt-label',
   templateUrl: './encapsulated.html'
 })
-export class LabelComponent implements OnInit {
+export class LabelComponent {
 
   @HostBinding('attr.host') readonly host = 'jnt-label-host';
 
@@ -18,10 +21,24 @@ export class LabelComponent implements OnInit {
   @HostBinding('style.background-color')
   @Input() color: string = Colors.purpleDark;
 
+  @HostBinding('style.color')
+  get textColor() {
+    return this.brightness(this.color) >= 150 ? DARK_COLOR : LIGHT_COLOR;
+  }
+
   constructor() {
   }
 
-  ngOnInit() {
+  private brightness(color: string): number {
+    color = color.substr(1);
+    if (color.length === 3) {
+      color = color.split('').map(v => v + v).join('');
+    }
+
+    const ratioRGB = [299, 587, 114];
+    const rgb = [0, 0, 0];
+    return rgb.map((v, i) => (ratioRGB[i] * parseInt(color.substr(i * 2, 2), 16)) / 1000)
+      .reduce((a, c) => a + c);
   }
 
 }
