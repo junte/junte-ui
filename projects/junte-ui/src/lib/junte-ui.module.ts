a8f2f6f0538b600e609c44b97da1e11f3d285bbc
rgromov <hanaevroman@gmail.com>
2019-07-01 16:32:43 +0300

    Fix themes

--- projects/junte-ui/src/lib/junte-ui.module.ts
+++ projects/junte-ui/src/lib/junte-ui.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ThemeManager } from 'projects/junte-ui/src/lib/managers/theme.manager';
 import { AppLayoutModule } from './components/app-layout/app-layout.module';
 import { AvatarModule } from './components/avatar/avatar.module';
 import { BadgeModule } from './components/badge/badge.module';
@@ -82,9 +81,6 @@ import { TextPipeModule } from './pipes/text-pipe.module';
     TableModule,
     TabsModule,
     PopoverModule
-  ],
-  providers: [
-    ThemeManager
   ]
 })
 export class JunteUiModule {
