09fe2af369dd29887f51bd45253393279600b839
juliaselezneva <juli.89@mail.ru>
2019-07-18 18:05:09 +0300

    #287 insert logo & favicon

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,6 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ModalComponent, ModalService, PopoverComponent, PopoverService, UI } from 'junte-ui';
 
+enum Themes {
+  light = 'light',
+  dark = 'dark'
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +13,19 @@ import { ModalComponent, ModalService, PopoverComponent, PopoverService, UI } fr
 })
 export class AppComponent implements AfterViewInit {
 
+  private _theme = Themes.light;
   ui = UI;
   loading: { [name: string]: boolean } = {};
+  themes = Themes;
+
+  set theme(theme: Themes) {
+    this._theme = theme;
+    this.load(theme);
+  }
+
+  get theme() {
+    return this._theme;
+  }
 
   @ViewChild('popover') popover: PopoverComponent;
   @ViewChild('modal') modal: ModalComponent;
@@ -24,7 +40,7 @@ export class AppComponent implements AfterViewInit {
     this.popoverService.register(this.popover);
   }
 
-  setTheme(theme: string = null) {
+  private load(theme: Themes) {
     this.loading[theme] = true;
     window['themes'](theme, () => this.loading[theme] = false);
   }
