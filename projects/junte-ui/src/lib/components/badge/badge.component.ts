137c0b774134fe27fabfea95b46f90ed428ddf27
juliaselezneva <juli.89@mail.ru>
2019-08-02 17:52:40 +0300

    #350 pass color badge

--- projects/junte-ui/src/lib/components/badge/badge.component.ts
+++ projects/junte-ui/src/lib/components/badge/badge.component.ts
@@ -1,29 +1,46 @@
-import {Component, HostBinding, Input, OnInit} from '@angular/core';
-import {Positions, Schemes} from '../../enum/ui';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Colors, Positions, Schemes } from '../../enum/ui';
+
+const LIGHT_COLOR = '#FFF';
+const DARK_COLOR = '#4F4F4F';
+const RATIO_BRIGHTNESS = 150;
+const RATIO_RGB = [299, 587, 114];
 
 @Component({
   selector: 'jnt-badge',
   templateUrl: './badge.encapsulated.html'
 })
-export class BadgeComponent implements OnInit {
+export class BadgeComponent {
 
   @HostBinding('attr.host') readonly host = 'jnt-badge-host';
 
   @Input()
   count: string;
 
-  @HostBinding('attr.scheme')
-  @Input()
-  scheme: Schemes = Schemes.primary;
-
   @HostBinding('attr.position')
   @Input()
   position: Positions = Positions.rightTop;
 
+  @HostBinding('style.background-color')
+  @Input() color: string = Colors.purple;
+
+  @HostBinding('style.color')
+  get textColor() {
+    return this.brightness(this.color) >= RATIO_BRIGHTNESS ? DARK_COLOR : LIGHT_COLOR;
+  }
+
   constructor() {
   }
 
-  ngOnInit() {
+  private brightness(color: string): number {
+    color = color.substr(1);
+    if (color.length === 3) {
+      color = color.split('').map(v => v + v).join('');
+    }
+
+    const rgb = [0, 0, 0];
+    return rgb.map((v, i) => (RATIO_RGB[i] * parseInt(color.substr(i * 2, 2), 16)) / 1000)
+      .reduce((a, c) => a + c);
   }
 
 }
