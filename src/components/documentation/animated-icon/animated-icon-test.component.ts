024f4e6c4ef78e711830585af1c81b7131a9d38b
rgromov <hanaevroman@gmail.com>
2019-08-28 11:03:06 +0300

    Refactoring

--- src/components/documentation/animated-icon/animated-icon-test.component.ts
+++ src/components/documentation/animated-icon/animated-icon-test.component.ts
@@ -1,15 +1,10 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-animated-icon-test',
   templateUrl: './animated-icon-test.component.html',
   styleUrls: ['./animated-icon-test.component.scss']
 })
-export class AnimatedIconTestComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AnimatedIconTestComponent {
 
 }
