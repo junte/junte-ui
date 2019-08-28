8db53c899216a6ee0fbaa522fdebbf955c8c9d64
Roman Gromov <hanaevroman@gmail.com>
2019-08-28 11:23:46 +0000

    Resolve "Animated icon component"

---
+++ projects/junte-ui/src/lib/components/animated-icon/animated-icon.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jnt-animated-icon',
+  templateUrl: './animated-icon.component.html',
+  styleUrls: ['./animated-icon.component.scss']
+})
+export class AnimatedIconComponent implements OnInit {
+
+  private source: string;
+  @Input() icon: string;
+  @Input() iconset: string = '';
+
+  ngOnInit() {
+    this.source = `assets/icons/animated/${!!this.iconset ? this.iconset + '/' : ''}${this.icon}.svg`;
+  }
+}
