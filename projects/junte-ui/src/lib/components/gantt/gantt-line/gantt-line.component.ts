758e71e12e3e6f679f1be9bf12e2a3a28a0da6df
rgromov <hanaevroman@gmail.com>
2019-09-19 16:03:14 +0300

    update gantt component

--- projects/junte-ui/src/lib/components/gantt/gantt-line/gantt-line.component.ts
+++ projects/junte-ui/src/lib/components/gantt/gantt-line/gantt-line.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
+import { Component, ContentChild, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'jnt-gantt-line',
@@ -11,4 +11,5 @@ export class GanttLineComponent {
   @Input() to: Date;
   @Input() period: any;
   @ContentChild('indicator', {static: false}) indicatorTemplate: TemplateRef<any>;
+  @Output() click = new EventEmitter<any>();
 }
