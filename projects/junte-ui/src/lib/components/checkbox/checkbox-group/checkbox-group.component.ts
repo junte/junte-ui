63dc1513c23c82c33d7a09d4e41f28e8b9c83f3c
Julia Selezneva <juli.89@mail.ru>
2019-11-18 12:12:17 +0000

    Resolve "Improve checkbox component"

--- projects/junte-ui/src/lib/components/checkbox/checkbox-group/checkbox-group.component.ts
+++ projects/junte-ui/src/lib/components/checkbox/checkbox-group/checkbox-group.component.ts
@@ -1,8 +1,7 @@
 import { AfterContentInit, Component, ContentChildren, forwardRef, HostBinding, OnDestroy, QueryList } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { fromEvent, merge, Subscription } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
-import { isArray } from 'util';
 import { CheckboxComponent } from '../checkbox.component';
 
 @Component({
@@ -17,7 +16,7 @@ import { CheckboxComponent } from '../checkbox.component';
   ]
 })
 
-export class CheckboxGroupComponent implements AfterContentInit, OnDestroy {
+export class CheckboxGroupComponent implements AfterContentInit, OnDestroy, ControlValueAccessor {
 
   @HostBinding('attr.host') readonly host = 'jnt-checkbox-group-host';
 
@@ -28,9 +27,7 @@ export class CheckboxGroupComponent implements AfterContentInit, OnDestroy {
 
   set selectedItems(value: any) {
     this._selectedItems = value;
-
     this.updateSelected();
-    this.onChange(value);
   }
 
   get selectedItems() {
@@ -39,10 +36,12 @@ export class CheckboxGroupComponent implements AfterContentInit, OnDestroy {
 
   ngAfterContentInit() {
     const changes$ = this.listCheckboxComponent.map(checkbox =>
-      fromEvent(checkbox.getElement(), 'click').pipe(mapTo(checkbox)));
+      fromEvent(checkbox.element.nativeElement, 'click').pipe(mapTo(checkbox)));
 
-    this.subscribe = merge(...changes$).subscribe((ckeckbox: CheckboxComponent) =>
-      this.select(ckeckbox.value));
+    this.subscribe = merge(...changes$)
+      .subscribe((checkbox: CheckboxComponent) => this.select(checkbox.value));
+
+    this.updateSelected();
   }
 
   ngOnDestroy() {
@@ -50,23 +49,25 @@ export class CheckboxGroupComponent implements AfterContentInit, OnDestroy {
   }
 
   private updateSelected() {
-    this.listCheckboxComponent.forEach(r => r.checked = this.selectedItems.includes(r.value));
+    if (!!this.listCheckboxComponent) {
+      this.listCheckboxComponent.forEach(checkbox =>
+        checkbox.checked = this.selectedItems.includes(checkbox.value));
+    }
   }
 
   select(value) {
-    const index = this.selectedItems.findIndex(i => i === value);
+    const index = this.selectedItems.findIndex(item => item === value);
     if (index > -1) {
       this.selectedItems.splice(index, 1);
     } else {
       this.selectedItems.push(value);
     }
-
     this.onChange(this.selectedItems);
   }
 
   writeValue(value: any) {
     if (!!value) {
-      this.selectedItems = isArray(value) ? value : [value];
+      this.selectedItems = Array.isArray(value) ? value : [value];
     }
   }
 
