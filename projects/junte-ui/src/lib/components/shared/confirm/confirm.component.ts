ca1644f6f582350a16de2545ba819d0137944ab2
juliaselezneva <juli.89@mail.ru>
2019-12-13 17:24:06 +0300

    #607 confirm component

--- projects/junte-ui/src/lib/components/shared/confirm/confirm.component.ts
+++ projects/junte-ui/src/lib/components/shared/confirm/confirm.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, HostBinding, Input, Output, TemplateRef } from '@angular/core';
 import { UI } from '../../../enum/ui';
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'jnt-confirm',
@@ -9,10 +8,9 @@ import { FormGroup } from '@angular/forms';
 export class ConfirmComponent {
 
   ui = UI;
-  form: FormGroup;
   @HostBinding('attr.host') readonly host = 'jnt-confirm-host';
-  @Input() message: string | TemplateRef<any>;
-  @Input() loading: boolean;
+  @Input() message: string;
+  @Input() messageTemplate: TemplateRef<any>;
   @Output() ok = new EventEmitter();
   @Output() cancel = new EventEmitter();
 
--- src/components/documentation/shared/confirm/confirm-test.component.ts
+++ src/components/documentation/shared/confirm/confirm-test.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { UI } from '../../../../../projects/junte-ui/src/lib/enum/ui';
 
 @Component({
@@ -10,19 +10,8 @@ export class ConfirmTestComponent {
 
   ui = UI;
 
-  @ViewChild('message', {static: false})
-  message: TemplateRef<any>;
-
-  loading = false;
-
-  load() {
-    this.loading = true;
-    setTimeout(() => this.loading = false, 2000);
-  }
-
   success() {
     console.log('success');
-    this.load();
   }
 
   fail() {
