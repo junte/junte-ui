3563c8ac2cd9457c84320caacfd2ea3a4d86bcca
juliaselezneva <juli.89@mail.ru>
2019-12-19 19:17:21 +0300

    #620 app-layout test page

--- src/components/documentation/layout/app-layout/app-layout-test.component.ts
+++ src/components/documentation/layout/app-layout/app-layout-test.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UI } from 'junte-ui';
+import { LocalUI } from '../../../../enums/local-ui';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-app-layout-test',
   templateUrl: './app-layout-test.component.html',
   styleUrls: ['./app-layout-test.component.scss']
 })
-export class AppLayoutTestComponent implements OnInit {
+export class AppLayoutTestComponent {
+
   ui = UI;
+  localUi = LocalUI;
 
-  constructor() {
-  }
+  header = new FormControl(true);
+  logotype = new FormControl(true);
+  menu = new FormControl(true);
+  action = new FormControl(true);
+  usermenu = new FormControl(true);
 
-  ngOnInit() {
+  form = this.fb.group({
+    header: this.header,
+    logotype: this.logotype,
+    menu: this.menu,
+    action: this.action,
+    usermenu: this.usermenu,
+  });
+
+  constructor(private fb: FormBuilder) {
   }
 
 }
