4bf6c85e2c1f3e9c0780254bc10270a7c512dc4c
hanaevroman@gmail.com <hanaevroman@gmail.com>
2020-02-14 15:19:24 +0300

    #774 fix error

--- src/components/documentation/shared/browser-preview/browser-preview.component.ts
+++ src/components/documentation/shared/browser-preview/browser-preview.component.ts
@@ -1,23 +1,31 @@
-import { ChangeDetectorRef, Component, ContentChild, ElementRef, HostListener, TemplateRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ContentChild, ElementRef, HostListener, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-browser-preview',
   templateUrl: './browser-preview.component.html',
   styleUrls: ['./browser-preview.component.scss']
 })
-export class BrowserPreviewComponent {
+export class BrowserPreviewComponent implements AfterViewInit {
+
+  width: number;
+  height: number;
 
   @ContentChild('document', {static: false})
   documentTemplate: TemplateRef<any>;
 
   @HostListener('window:resize')
   resizing() {
-    this.cd.markForCheck();
+    this.cd.detectChanges();
   }
 
   constructor(private cd: ChangeDetectorRef,
               public host: ElementRef<HTMLElement>) {
+  }
 
+  ngAfterViewInit() {
+    this.width = this.host.nativeElement.clientWidth;
+    this.height = this.host.nativeElement.clientHeight;
+    this.cd.detectChanges();
   }
 
 }
