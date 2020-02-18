0172061e789cdfd9fd6b700b3bb554d445ffac56
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-02-18 22:00:59 +0300

    small select review

--- projects/junte-ui/src/lib/components/forms/radio/radio.module.ts
+++ projects/junte-ui/src/lib/components/forms/radio/radio.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StackModule } from '../../layout/stack/stack.module';
 import { RadioComponent } from './radio.component';
 import { RadioGroupComponent } from './radio-group/radio-group.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StackModule
   ],
   exports: [
     RadioComponent,
