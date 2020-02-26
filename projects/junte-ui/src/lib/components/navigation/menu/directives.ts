e538ac4ee0245eb8222c2e81e62f08123772c24a
hanaevroman@gmail.com <hanaevroman@gmail.com>
2020-02-26 13:19:01 +0300

    #775 fix submenus

--- projects/junte-ui/src/lib/components/navigation/menu/directives.ts
+++ projects/junte-ui/src/lib/components/navigation/menu/directives.ts
@@ -19,21 +19,6 @@ export class SubMenuItemsDirective {
   top: number;
 }
 
-@Directive({selector: '[jntSubMenuTitle]'})
-export class SubMenuTitleDirective {
-
-  private _active = false;
-
-  @HostBinding('attr.active')
-  get active() {
-    return this._active;
-  }
-
-  set active(active: boolean) {
-    this._active = active;
-  }
-}
-
 @Directive({selector: '[jntSubMenu]'})
 export class SubMenuDirective implements AfterViewInit {
 
@@ -42,9 +27,6 @@ export class SubMenuDirective implements AfterViewInit {
 
   @ContentChild(SubMenuItemsDirective)
   submenu: SubMenuItemsDirective;
-
-  @ContentChild(SubMenuTitleDirective)
-  title: SubMenuTitleDirective;
 
   @ContentChildren(LinkComponent, {descendants: true})
   links: QueryList<LinkComponent>;
