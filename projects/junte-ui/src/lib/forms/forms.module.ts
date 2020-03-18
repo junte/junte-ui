1e63052b2983d94f1d1bc827cca85fabdd732e2e
Anton Khrypko <joefamwork@gmail.com>
2020-03-18 09:02:02 +0000

    Resolve "Move 'dot' from form to elements"

--- projects/junte-ui/src/lib/forms/forms.module.ts
+++ projects/junte-ui/src/lib/forms/forms.module.ts
@@ -4,7 +4,6 @@ import { ButtonModule } from './button/button.module';
 import { CalendarModule } from './calendar/calendar.module';
 import { CheckboxModule } from './checkbox/checkbox.module';
 import { DatePickerModule } from './date-picker/date-picker.module';
-import { DotModule } from './dot/dot.module';
 import { FormModule } from './form/form.module';
 import { InputModule } from './input/input.module';
 import { RadioModule } from './radio/radio.module';
@@ -18,7 +17,6 @@ import { SwitcherModule } from './switcher/switcher.module';
     CalendarModule,
     CheckboxModule,
     DatePickerModule,
-    DotModule,
     FormModule,
     InputModule,
     RadioModule,
