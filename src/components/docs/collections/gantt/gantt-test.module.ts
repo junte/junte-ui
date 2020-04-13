0dbc65306c9496a9eeade596f0b297c450a83f02
juliaselezneva <juli.89@mail.ru>
2020-04-13 10:11:16 +0300

    #935 move to data-attr selectors

--- src/components/docs/collections/gantt/gantt-test.module.ts
+++ src/components/docs/collections/gantt/gantt-test.module.ts
@@ -27,11 +27,11 @@ import { GanttTestComponent } from './gantt-test.component';
     GridModule.forRoot(JUNTE_UI_CONFIG),
     AccordionModule.forRoot(JUNTE_UI_CONFIG),
     FormModule.forRoot(JUNTE_UI_CONFIG),
+    SharedModule,
     GanttModule.forRoot(JUNTE_UI_CONFIG),
     ButtonModule.forRoot(JUNTE_UI_CONFIG),
     BlockModule.forRoot(JUNTE_UI_CONFIG),
-    DatePipesModule,
-    SharedModule
+    DatePipesModule
   ],
   exports: [
     GanttTestComponent
