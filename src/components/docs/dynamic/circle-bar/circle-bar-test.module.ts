58989fb0b3a6cfd6ce0ab1cc5b7b5a0c08d3cbbf
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-04-15 13:04:05 +0300

    circle bar core review

--- src/components/docs/dynamic/circle-bar/circle-bar-test.module.ts
+++ src/components/docs/dynamic/circle-bar/circle-bar-test.module.ts
@@ -1,7 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AccordionModule, CircleBarModule, FormModule, GridModule, InputModule, LinkModule, StackModule, TabsModule } from 'junte-ui';
+import {
+  IconModule,
+  AccordionModule,
+  CircleBarModule,
+  FormModule,
+  GridModule,
+  InputModule,
+  LinkModule,
+  StackModule,
+  TabsModule,
+  AvatarModule
+} from 'junte-ui';
 import { JUNTE_UI_CONFIG } from 'src/consts';
 import { SharedModule } from '../../shared/shared.module';
 
@@ -11,15 +22,17 @@ import { CircleBarTestComponent } from './circle-bar-test.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    IconModule.forRoot(JUNTE_UI_CONFIG),
+    AvatarModule.forRoot(JUNTE_UI_CONFIG),
     LinkModule.forRoot(JUNTE_UI_CONFIG),
     StackModule.forRoot(JUNTE_UI_CONFIG),
     TabsModule.forRoot(JUNTE_UI_CONFIG),
     GridModule.forRoot(JUNTE_UI_CONFIG),
     AccordionModule.forRoot(JUNTE_UI_CONFIG),
     FormModule.forRoot(JUNTE_UI_CONFIG),
-    SharedModule,
     CircleBarModule.forRoot(JUNTE_UI_CONFIG),
     FormsModule,
+    SharedModule,
     InputModule.forRoot(JUNTE_UI_CONFIG)
   ],
   exports: [
