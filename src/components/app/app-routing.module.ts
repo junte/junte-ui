6ea417ef7f80a912a65d5a7fd057ffae510b3099
Breslavsky Anton <breslavsky.anton@gmail.com>
2020-04-17 10:53:09 +0000

    Revert "Merge branch '957-preloader-for-all-modules' into 'master'"
    This reverts merge request !814

--- src/components/app/app-routing.module.ts
+++ src/components/app/app-routing.module.ts
@@ -1,42 +1,21 @@
 import { NgModule } from '@angular/core';
-import { PreloadingStrategy, Route, RouterModule, Routes } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { delay, mergeMap } from 'rxjs/operators';
-import { loadChildren } from 'src/utils/routing';
-
-const PRELOADING_DELAY = 15000;
+import { RouterModule, Routes } from '@angular/router';
+import { loadChildren } from '../../utils/routing';
 
 export function docsMatcher() {
   return {consumed: []};
 }
 
-export class DelayedModulePreloading implements PreloadingStrategy {
-  preloadedModules: string[] = [];
-
-  preload(route: Route, load: () => Observable<boolean>): Observable<boolean> {
-    if (route.data && route.data['preload']) {
-      // console.log(`preload module ${route.path}`);
-      this.preloadedModules.push(route.path);
-      return of(true).pipe(
-        delay(PRELOADING_DELAY),
-        mergeMap(() => load()));
-    } else {
-      return of(null);
-    }
-  }
-}
-
-
 const routes: Routes = [
   {
     path: '',
-    data: {breadcrumb: 'Home', preload: true},
+    data: {breadcrumb: 'Home'},
     loadChildren: () => loadChildren(import('../home/home.module')
       .then(m => m.HomeModule))
   },
   {
     matcher: docsMatcher,
-    data: {breadcrumb: 'Junte UI', preload: true},
+    data: {breadcrumb: 'Junte UI'},
     loadChildren: () => loadChildren(import('../docs/docs.module')
       .then(m => m.DocsModule))
   },
@@ -48,12 +27,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: DelayedModulePreloading,
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled',
     scrollOffset: [0, 64]
   })],
-  providers: [DelayedModulePreloading],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
