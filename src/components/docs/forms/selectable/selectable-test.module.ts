9ea344f32fce5882a135fecb7c7cd76e4a809951
hanaevroman@gmail.com <hanaevroman@gmail.com>
2020-05-14 10:39:29 +0300

    #1019 selectable directive

---
+++ src/components/docs/forms/selectable/selectable-test.module.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  AccordionModule,
+  AvatarModule, BlockModule,
+  ButtonModule,
+  CheckboxModule,
+  FormModule,
+  GridModule,
+  IconModule,
+  InputModule,
+  LabelModule,
+  LinkModule,
+  SelectableDirectiveModule,
+  SelectModule,
+  StackModule,
+  TabsModule
+} from 'junte-ui';
+import { SharedModule } from 'src/components/docs/shared/shared.module';
+import { JUNTE_UI_CONFIG } from 'src/consts';
+import { SelectableTestComponent } from './selectable-test.component';
+
+
+@NgModule({
+  declarations: [SelectableTestComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BlockModule.forRoot(JUNTE_UI_CONFIG),
+    ButtonModule.forRoot(JUNTE_UI_CONFIG),
+    IconModule.forRoot(JUNTE_UI_CONFIG),
+    LinkModule.forRoot(JUNTE_UI_CONFIG),
+    StackModule.forRoot(JUNTE_UI_CONFIG),
+    TabsModule.forRoot(JUNTE_UI_CONFIG),
+    GridModule.forRoot(JUNTE_UI_CONFIG),
+    FormModule.forRoot(JUNTE_UI_CONFIG),
+    AccordionModule.forRoot(JUNTE_UI_CONFIG),
+    GridModule.forRoot(JUNTE_UI_CONFIG),
+    SelectModule.forRoot(JUNTE_UI_CONFIG),
+    AvatarModule.forRoot(JUNTE_UI_CONFIG),
+    SharedModule,
+    CheckboxModule.forRoot(JUNTE_UI_CONFIG),
+    InputModule.forRoot(JUNTE_UI_CONFIG),
+    LabelModule.forRoot(JUNTE_UI_CONFIG),
+    SelectableDirectiveModule
+  ]
+})
+export class SelectableTestModule {
+}
