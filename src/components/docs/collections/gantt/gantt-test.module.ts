e9152aad0a66da54d3d75fcb4f218ccade01e91c
Anton Khrypko <joefamwork@gmail.com>
2020-06-03 11:07:34 +0000

    Resolve "Year view for gantt 2"

--- src/components/docs/collections/gantt/gantt-test.module.ts
+++ src/components/docs/collections/gantt/gantt-test.module.ts
@@ -6,13 +6,14 @@ import {
   IconModule,
   BlockModule,
   ButtonModule,
-  DatePipesModule,
   FormModule,
   GanttModule,
   GridModule,
+  GanttPipesModule,
   LinkModule,
   StackModule,
-  TabsModule
+  TabsModule,
+  SwitcherModule
 } from 'junte-ui';
 import { DateFnsModule } from 'ngx-date-fns';
 import { JUNTE_UI_CONFIG } from 'src/consts';
@@ -20,23 +21,25 @@ import { SharedModule } from '../../shared/shared.module';
 import { GanttTestComponent } from './gantt-test.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    IconModule.forRoot(JUNTE_UI_CONFIG),
-    LinkModule.forRoot(JUNTE_UI_CONFIG),
-    StackModule.forRoot(JUNTE_UI_CONFIG),
-    TabsModule.forRoot(JUNTE_UI_CONFIG),
-    GridModule.forRoot(JUNTE_UI_CONFIG),
-    AccordionModule.forRoot(JUNTE_UI_CONFIG),
-    FormModule.forRoot(JUNTE_UI_CONFIG),
-    GanttModule.forRoot(JUNTE_UI_CONFIG),
-    ButtonModule.forRoot(JUNTE_UI_CONFIG),
-    BlockModule.forRoot(JUNTE_UI_CONFIG),
-    DatePipesModule,
-    SharedModule,
-    DateFnsModule
-  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        IconModule.forRoot(JUNTE_UI_CONFIG),
+        LinkModule.forRoot(JUNTE_UI_CONFIG),
+        StackModule.forRoot(JUNTE_UI_CONFIG),
+        TabsModule.forRoot(JUNTE_UI_CONFIG),
+        GridModule.forRoot(JUNTE_UI_CONFIG),
+        AccordionModule.forRoot(JUNTE_UI_CONFIG),
+        FormModule.forRoot(JUNTE_UI_CONFIG),
+        GanttModule.forRoot(JUNTE_UI_CONFIG),
+        ButtonModule.forRoot(JUNTE_UI_CONFIG),
+        BlockModule.forRoot(JUNTE_UI_CONFIG),
+        SwitcherModule.forRoot(JUNTE_UI_CONFIG),
+        GanttPipesModule,
+        SharedModule,
+        DateFnsModule,
+        StackModule
+    ],
   exports: [
     GanttTestComponent
   ],
