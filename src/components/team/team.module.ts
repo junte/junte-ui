294e255da9da86485492fe58fda3f763217c7422
juliaselezneva <juli.89@mail.ru>
2020-07-10 09:38:36 +0300

    #1237 fix team page

--- src/components/team/team.module.ts
+++ src/components/team/team.module.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { AvatarModule } from '../../../projects/junte-ui/src/lib/elements/avatar/avatar.module';
-import { GridModule } from '../../../projects/junte-ui/src/lib/layout/grid/grid.module';
-import { StackModule } from '../../../projects/junte-ui/src/lib/layout/stack/stack.module';
-import { LinkModule } from '../../../projects/junte-ui/src/lib/navigation/link/link.module';
+import { AvatarModule, GridModule, StackModule, LinkModule } from 'junte-ui';
 import { AppFooterModule } from '../footer/app-footer.module';
 import { TeamRoutingModule } from './team-routing.module';
 import { TeamComponent } from './team.component';
