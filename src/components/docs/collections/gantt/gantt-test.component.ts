16e7eb4a1e583c11d268fc2b67f43a50c51377cb
joefamwork <joefamwork@gmail.com>
2020-08-18 13:56:42 +0300

    #970

--- src/components/docs/collections/gantt/gantt-test.component.ts
+++ src/components/docs/collections/gantt/gantt-test.component.ts
@@ -45,9 +45,10 @@ export class GanttTestComponent implements OnInit {
       login: faker.name.findName()
     },
     periods: [
-      {from: new Date('2020-07-02'), to: new Date('2020-08-05'), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
+      {from: new Date('2020-07-22'), to: new Date('2020-08-05'), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
       {from: new Date('2020-08-08'), to: new Date('2020-08-12'), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
       {from: new Date('2020-08-17'), to: new Date('2020-09-25'), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
+      // {from: new Date('2020-07-07'), to: new Date('2020-07-18'), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
       // {from: faker.date.recent(10), to: faker.date.recent(3), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
       // {from: faker.date.recent(-3), to: faker.date.recent(-10), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])},
      // {from: faker.date.recent(3), to: faker.date.recent(7), status: faker.helpers.randomize(['accepting', 'accepted', 'declined'])}
