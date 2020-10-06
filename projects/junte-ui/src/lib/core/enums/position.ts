1c4e5f4d45f58b4f7ad383a485a1aee3f01d78cf
Roman Gromov <hanaevroman@gmail.com>
2020-10-06 08:42:27 +0000

    Resolve "Improvement popover position"

--- projects/junte-ui/src/lib/core/enums/position.ts
+++ projects/junte-ui/src/lib/core/enums/position.ts
@@ -1,10 +1,12 @@
 export enum Position {
   left = 'left',
   right = 'right',
-  rightTop = 'rightTop',
-  leftTop = 'leftTop',
-  inline = 'inline',
   top = 'top',
   bottom = 'bottom',
+  rightTop = 'rightTop',
+  leftTop = 'leftTop',
+  rightBottom = 'rightBottom',
+  leftBottom = 'leftBottom',
+  inline = 'inline',
   center = 'center'
 }
