30eec574cf44b725f9c1c77a81542f5773906aff
Julia Selezneva <juli.89@mail.ru>
2020-11-09 14:05:29 +0000

    Resolve "Informer improvements"

--- projects/junte-ui/src/lib/layout/informer/informer.component.ts
+++ projects/junte-ui/src/lib/layout/informer/informer.component.ts
@@ -1,4 +1,16 @@
-import { Component, ContentChildren, ElementRef, EventEmitter, HostBinding, Input, Output, QueryList, Renderer2 } from '@angular/core';
+import {
+  Component, ContentChild,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  Input, OnDestroy,
+  Output,
+  QueryList,
+  Renderer2,
+  TemplateRef
+} from '@angular/core';
+import { Placement } from '../../core/enums/placement';
 import { PropertyApi } from '../../core/decorators/api';
 import { UI } from '../../core/enums/ui';
 
@@ -16,12 +28,26 @@ export class InformerMessageComponent {
   selector: 'jnt-informer',
   templateUrl: './informer.encapsulated.html'
 })
-export class InformerComponent {
+export class InformerComponent implements OnDestroy {
 
   ui = UI;
   _backdrop: ElementRef<HTMLElement>;
 
+  @HostBinding('attr.data-placement')
+  _placement: Placement = Placement.fixed;
+
   @HostBinding('attr.host') readonly host = 'jnt-informer-host';
+
+  @PropertyApi({
+    description: 'Informer placement',
+    path: 'ui.placement',
+    default: Placement.fixed,
+    options: [Placement.absolute, Placement.fixed]
+  })
+  @Input()
+  set placement(placement: Placement) {
+    this._placement = placement || Placement.fixed;
+  }
 
   @PropertyApi({
     description: 'Icon of informer',
@@ -48,15 +74,17 @@ export class InformerComponent {
   @ContentChildren(InformerMessageComponent, {descendants: true})
   messages: QueryList<InformerMessageComponent>;
 
+  @ContentChild('informerContentTemplate')
+  contentTemplate: TemplateRef<any>;
+
   @Output() ok = new EventEmitter();
 
   constructor(private render: Renderer2) {
   }
 
-  close() {
+  ngOnDestroy() {
     if (!!this.backdrop) {
       this.render.removeStyle(this.backdrop.nativeElement, 'filter');
     }
-    this.ok.emit();
   }
 }
