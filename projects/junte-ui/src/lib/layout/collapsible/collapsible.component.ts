f3deeac68c27de8ab6f369eac800c0729deaf4d7
Julia Selezneva <juli.89@mail.ru>
2020-11-24 12:51:51 +0000

    Resolve "Orientation for collapsed"

--- projects/junte-ui/src/lib/layout/collapsible/collapsible.component.ts
+++ projects/junte-ui/src/lib/layout/collapsible/collapsible.component.ts
@@ -1,4 +1,5 @@
 import { Component, ContentChild, HostBinding, Input, TemplateRef } from '@angular/core';
+import { Orientation } from '../../core/enums/orientation';
 import { ContentApi, PropertyApi } from '../../core/decorators/api';
 import { UI } from '../../core/enums/ui';
 
@@ -11,6 +12,23 @@ export class CollapsibleComponent {
   @HostBinding('attr.host') readonly host = 'jnt-collapsible-host';
 
   ui = UI;
+
+  @HostBinding('attr.data-orientation')
+  _orientation: Orientation = Orientation.horizontal;
+
+  @PropertyApi({
+    description: 'Collapsible orientation',
+    path: 'ui.orientation',
+    default: Orientation.horizontal,
+    options: [Orientation.horizontal, Orientation.vertical]
+  })
+  @Input() set orientation(type: Orientation) {
+    this._orientation = type || Orientation.horizontal;
+  }
+
+  get orientation() {
+    return this._orientation;
+  }
 
   @PropertyApi({
     description: 'opened of collapsible',
