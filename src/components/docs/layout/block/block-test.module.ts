8106641c5fdd02fb60116f13d82a77e3d512d2ab
Dmitriy Yashin <chiflash101@gmail.com>
2021-01-12 11:39:49 +0300

    Add custom template adapted for block #1803

--- src/components/docs/layout/block/block-test.module.ts
+++ src/components/docs/layout/block/block-test.module.ts
@@ -15,32 +15,34 @@ import {
   SkeletonModule,
   StackModule,
   SwitcherModule,
-  TabsModule
+  TabsModule,
+  PopoverModule
 } from 'junte-ui';
 import { SharedModule } from '../../shared/shared.module';
 import { BlockTestComponent } from './block-test.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    IconModule,
-    LinkModule,
-    StackModule,
-    TabsModule,
-    GridModule,
-    FormModule,
-    AccordionModule,
-    GridModule,
-    SkeletonModule,
-    ButtonModule,
-    SelectModule,
-    SwitcherModule,
-    BlockModule,
-    CheckboxModule,
-    SharedModule,
-    AppLayoutModule
-  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        IconModule,
+        LinkModule,
+        StackModule,
+        TabsModule,
+        GridModule,
+        FormModule,
+        AccordionModule,
+        GridModule,
+        SkeletonModule,
+        ButtonModule,
+        SelectModule,
+        SwitcherModule,
+        BlockModule,
+        CheckboxModule,
+        SharedModule,
+        AppLayoutModule,
+        PopoverModule
+    ],
   exports: [
     BlockTestComponent
   ],
