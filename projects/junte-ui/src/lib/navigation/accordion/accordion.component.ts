66e75404ad4259016e877d9960f3751ac904170c
juliaselezneva <juli.89@mail.ru>
2021-01-25 10:40:27 +0300

    Remove select and accordion animations #1876

--- projects/junte-ui/src/lib/navigation/accordion/accordion.component.ts
+++ projects/junte-ui/src/lib/navigation/accordion/accordion.component.ts
@@ -1,24 +1,10 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, ContentChildren, EventEmitter, HostBinding, Input, Output, QueryList } from '@angular/core';
 import { UI } from '../../core/enums/ui';
 import { AccordionSectionComponent } from './section/accordion-section.component';
 
 @Component({
   selector: 'jnt-accordion',
-  templateUrl: './accordion.encapsulated.html',
-  animations: [
-    trigger('rotate', [
-        state('open', style({transform: 'rotate(-90deg)'})),
-        state('close', style({transform: 'rotate(0)'})),
-        transition('open <=> close', [animate('.3s ease-in-out')]),
-      ]
-    ),
-    trigger('collapse', [
-      transition(':enter', [style({height: 0}), animate('.3s', style({height: '*'}))]),
-      transition(':leave', [style({height: '*'}), animate('.3s', style({height: 0}))]),
-      ]
-    )
-  ]
+  templateUrl: './accordion.encapsulated.html'
 })
 export class AccordionComponent {
 
