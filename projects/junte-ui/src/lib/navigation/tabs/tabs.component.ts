b1397fdea4a09fbb706db1c569756d787b900c05
Julia Selezneva <juli.89@mail.ru>
2021-02-26 15:56:42 +0000

    Resolve "Animation for tabs"

--- projects/junte-ui/src/lib/navigation/tabs/tabs.component.ts
+++ projects/junte-ui/src/lib/navigation/tabs/tabs.component.ts
@@ -1,56 +1,53 @@
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
-import { Component, ContentChildren, EventEmitter, HostBinding, Input, Output, QueryList } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
+  QueryList, Renderer2,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import { PropertyApi } from '../../core/decorators/api';
 import { Feature } from '../../core/enums/feature';
 import { Outline } from '../../core/enums/outline';
 import { UI } from '../../core/enums/ui';
 import { TabComponent } from './tab.component';
 
+// @ts-ignore
 @Component({
   selector: 'jnt-tabs',
   templateUrl: './tabs.encapsulated.html',
   animations: [
     trigger('flash', [
-        state(
-          'void',
-          style({
-            opacity: 0
-          })
-        ),
-        state(
-          '*',
-          style({
-            opacity: 1,
-            width: '150%',
-            height: '150%'
-          })
-        ),
-        transition(
-          'void => *',
-          [
-            animate('.4s ease-in-out')
-          ]
-        ),
-        transition(
-          '* => void',
-          [
-            animate('.3s ease-in-out', keyframes([
-              style({opacity: '0'}),
-            ]))
-          ]
-        ),
+        state('void', style({opacity: 0})),
+        state('*', style({opacity: 1, width: '150%', height: '150%'})),
+        transition('void => *', [animate('.4s ease-in-out')]),
+        transition('* => void', [animate('.3s ease-in-out', keyframes([style({opacity: '0'})]))]),
       ]
     ),
+    trigger('move', [
+      state('*', style({transform: 'translateX({{distance}}px)'}), {params: {distance: '0'}}),
+      transition('* <=> *', [animate('.4s ease-in-out')])
+    ])
   ]
 })
 export class TabsComponent {
 
   ui = UI;
   private _active = 0;
+  distance = 0;
+
+  @ViewChild('lineRef')
+  lineRef: ElementRef;
 
   @Input()
   set active(active: number) {
     this._active = active;
+    this.distance = this.links.toArray()[this.active].nativeElement.offsetLeft;
     this.changed.emit(active);
   }
 
@@ -88,4 +85,18 @@ export class TabsComponent {
 
   @ContentChildren(TabComponent)
   tabs: QueryList<TabComponent>;
+
+  @ViewChildren('links')
+  links: QueryList<ElementRef>;
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  setWidth(active: number) {
+    const tab = this.links.toArray()[active];
+    if (!!tab) {
+      this.renderer.setStyle(this.lineRef.nativeElement, 'width', tab.nativeElement.offsetWidth + 'px');
+    }
+  }
+
 }
