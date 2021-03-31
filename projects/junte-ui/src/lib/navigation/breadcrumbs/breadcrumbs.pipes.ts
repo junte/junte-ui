0035e5528d7635509f60be844c74fa36ab0eb5b9
Roman Gromov <gromov.r.v.92@gmail.com>
2021-03-31 09:32:19 +0000

    Resolve "Fix data-href in breadcrumbs"

---
+++ projects/junte-ui/src/lib/navigation/breadcrumbs/breadcrumbs.pipes.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ActivatedRouteSnapshot } from '@angular/router';
+
+@Pipe({name: 'getUrl'})
+export class GetUrlPipe implements PipeTransform {
+  transform(snapshot: ActivatedRouteSnapshot): string {
+    return snapshot.pathFromRoot
+      .map(route => route.url.map(url => url.path).join('/'))
+      .filter(path => !!path).join('/');
+  }
+}
