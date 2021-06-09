afd33173ebefa7e9f97e4b68a890141b794bf852
Roman Gromov <gromov.r.v.92@gmail.com>
2021-06-09 14:10:29 +0000

    Resolve "Update modal"

--- projects/junte-ui/src/lib/overlays/modal/public_api.ts
+++ projects/junte-ui/src/lib/overlays/modal/public_api.ts
@@ -1,4 +1,5 @@
-export { ModalComponent, ModalOptions } from './modal.component';
+export { ModalComponent } from './modal.component';
+export { ModalOptions } from './modal.types';
 export { ModalService } from './modal.service';
 export { ModalModule } from './modal.module';
 export { ModalDirective } from './modal.directive';
