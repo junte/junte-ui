55626a676f1901182f1242f479db053d47cf45da
Julia Selezneva <juli.89@mail.ru>
2021-06-29 09:12:44 +0000

    Resolve "Move viewport directive from SP"

--- projects/junte-ui/src/lib/layout/responsive/public_api.ts
+++ projects/junte-ui/src/lib/layout/responsive/public_api.ts
@@ -1,4 +1,5 @@
 export { BreakpointService } from './breakpoint.service';
 export { DeviceService } from './device.service';
 export { BreakpointDirective, ForDirective, ForMaxDirective, ForMinDirective} from './responsive.directives';
+export { ViewportDirective, ViewportRuleDirective } from './viewport.directive';
 export { ResponsiveModule } from './responsive.module';
