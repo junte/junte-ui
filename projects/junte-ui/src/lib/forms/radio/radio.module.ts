3ac844da0d36e00d11f07516a62ca74722e2c041
juliaselezneva <juli.89@mail.ru>
2021-08-11 16:41:44 +0300

    Add empty feature to radio #1913

--- projects/junte-ui/src/lib/forms/radio/radio.module.ts
+++ projects/junte-ui/src/lib/forms/radio/radio.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ArrayPipesModule } from '../../core/pipes/array-pipes.module';
 import { MathPipesModule } from '../../core/pipes/math-pipes.module';
 import { IconModule } from '../../elements/icon/icon.module';
 import { GridModule } from '../../layout/grid/grid.module';
@@ -15,7 +16,8 @@ import { RadioComponent } from './radio.component';
     StackModule,
     IconModule,
     GridModule,
-    MathPipesModule
+    MathPipesModule,
+    ArrayPipesModule
   ],
   exports: [
     RadioComponent,
